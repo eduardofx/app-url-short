801e471eb6655bdb6082e26eba04d639653e1ff0
Eduardo Kawassaki <eduardok.fx@gmail.com>
2020-06-14 10:32:25 -0300

    port

--- config/config.production.ts
+++ config/config.production.ts
@@ -2,7 +2,7 @@ export const config = {
     database: {
         dialect: 'postgres',
         host: process.env.DATABASE_HOST,
-        port: process.env.DATABASE_PORT,
+        port: 5432,
         username: process.env.DATABASE_USER,
         password: process.env.DATABASE_PASSWORD,
         database: process.env.DATABASE_DATABASE,
